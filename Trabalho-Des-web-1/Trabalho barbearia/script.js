bb50d0d0d2b35fc5a574ce56c9dc039e6a11c1b8
euaquitlgd? <evillyn.santos.tds2023@gmail.com>
2024-12-14 19:03:50 -0300

    e

--- Trabalho-Des-web-1/Trabalho barbearia/script.js
+++
@@ -1,8 +0,0 @@
-const splash = document.querySelector("#splash");
-
-document.addEventListener("DOMContentLoaded", (e) => {
-    setTimeout(() => {
-        document.getElementById("splash").style.display = "none";
-        document.location.href = "home.html";
-    }, 5000);
-})
